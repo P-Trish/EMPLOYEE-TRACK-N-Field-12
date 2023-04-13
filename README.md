69a90993e81da171a87ea0f40b9fa9258e3863e9
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-04-12 21:29:36 -0700

    added schema and seeds

--- README.md
+++ README.md
@@ -20,7 +20,11 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+npm i
+npm i inquirer@8.2.4
+npm i mysql2
+npm i console.table
+npm i dotenv
 
 ## Usage
 
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name) 
+VALUES  ("Production Office"),
+        ("Writers"),
+        ("Post-Production");
+        
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Production Manager", 250000, 1),
+        ("Show Runner", 500000, 2),
+        ("Staff Writer", 125000, 2),
+        ("Editor", 150000, 3 ),
+        ("Production Coordinator", 65000, 1)
+        ("Post Supervisor", 87000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES  ("Liziel", "Corate", 1, null),
+        ("Izarra", "Alberto", 5, 1),
+        ("Jamie", "Dino", 2, null),
+        ("Levi", "Celestino",3, 3),
+        ("Emily", "Cronk", 6, null),
+        ("Felisa", "Vinluan", 4, 5);
+
+
+
+    
